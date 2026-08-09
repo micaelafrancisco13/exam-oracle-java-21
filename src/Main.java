382d10c86d22a291db1d23c0bc903065858d61ec
Ela <micaelafrancisco13@gmail.com>
2026-08-09 16:21:48 +0800

    phase 0 module

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        printFiveNumbers();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Max: " + returnMax(1, 2));
+
+        printList();
+
+        var book = new Book("Java 101", 100);
+        System.out.println("Book title: " + book.title());
+
+        divisionByZero();
+    }
+
+    private static void divisionByZero() {
+        int a = 10;
+        int b = 0;
+        try {
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            System.out.println("Division by zero is not allowed");
         }
+    }
+
+    private static void printList() {
+        var list = new ArrayList<>(List.of("red", "green", "blue"));
+        list.add("yellow");
+        list.forEach(System.out::println);
+    }
+
+    private static int returnMax(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    private static void printFiveNumbers() {
+        for (int i = 1; i <= 5; ++i)
+            System.out.println(i);
+    }
+
+    public record Book(String title, int pages) {
     }
 }
